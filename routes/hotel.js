017091953b3fa71005aac42910b36f63e941d12b
tientoantai <nguyenminhtien61vn@gmail.com>
2017-04-03 17:03:24 +0700

    route

---
+++ routes/hotel.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/hotel', function(req, res, next) {
+    //TODO
+});
+
+module.exports = router;
---
+++ routes/room.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/rooms/:hotelId', function(req, res, next) {
+    //TODO
+});
+
+router.get('/lowestPrice/:hotelId', function(req, res, next) {
+    //TODO
+});
+
+router.post('/room', function(req, res, next) {
+    //TODO
+});
+
+module.exports = router;
