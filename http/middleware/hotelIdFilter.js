a3cc389c45f50846685430b0b6536fbe1c265005
tientoantai <nguyenminhtien61vn@gmail.com>
2017-04-05 14:28:07 +0700

    hotelId data filter

--- http/middleware/hotelIdFilter.js
+++ http/middleware/hotelIdFilter.js
@@ -3,7 +3,8 @@
 var hotelRepo = require('../../app/Hotel/HotelRepository');
 
 module.exports = function (req, res, next) {
-    hotelRepo.prototype.get(req.params.hotelId).then(
+    var hotel_id = req.params.hotelId ? req.params.hotelId : req.body.hotel_id;
+    hotelRepo.prototype.get(hotel_id).then(
         function(result){
             if ( result.length == 0){
                 return res.status(400).json({"message": "The hotel is not exist"})
